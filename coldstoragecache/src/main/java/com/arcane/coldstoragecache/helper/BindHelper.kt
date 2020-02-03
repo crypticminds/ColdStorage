2f057eeff3f8bd5ff1f7f487182fb12363a9b970
crypticminds <50552150+crypticminds@users.noreply.github.com>
2020-02-03 15:29:46 +0530

    Image cache annotation

--- coldstoragecache/src/main/java/com/arcane/coldstoragecache/helper/BindHelper.kt
+++ coldstoragecache/src/main/java/com/arcane/coldstoragecache/helper/BindHelper.kt
@@ -4,6 +4,7 @@ import android.animation.ValueAnimator
 import android.app.Activity
 import android.view.View
 import android.widget.ImageView
+import androidx.fragment.app.Fragment
 
 class BindHelper {
 
@@ -17,12 +18,20 @@ class BindHelper {
                 is View -> {
                     bindView(anyObject, resourceId)
                 }
-//                is Fragment -> {
-//                    null
-//                }
+                is Fragment -> {
+                    bindView(anyObject, resourceId)
+                }
                 else -> {
                     throw Exception("Only views , activities and fragments are supported for the annotation")
                 }
+            }
+        }
+
+        private fun bindView(fragment: Fragment, resourceId: Int): ImageView {
+            if (fragment.view != null) {
+                return fragment.view!!.findViewById(resourceId)
+            } else {
+                throw Exception("Unable to get the root view of the fragment ${fragment.javaClass.simpleName}")
             }
         }
 
